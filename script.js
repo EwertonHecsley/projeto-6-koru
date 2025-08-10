a79cf7d5f0e08d4875eacd9215b6e17cc21dc41a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-08-09 23:45:38 -0300

    funcao para adicionar texto

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const apiKeyInput = document.getElementById("apiKey");
 const userQuestionInput = document.getElementById("userQuestion");
 const chatContainer = document.getElementById("chat");
 const form = document.querySelector(".chat-input");
+
+function addMessage(text, sender) {
+    const msg = document.createElement("div");
+    msg.classList.add("mensagem", sender);
+    msg.textContent = text;
+    chatContainer.appendChild(msg);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
