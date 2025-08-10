90aaf1a6c723383ee0c06ef711bf755b0cd41821
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-08-09 23:44:05 -0300

    Pegar referências aos inputs

---
+++ script.js
@@ -0,0 +1,4 @@
+const apiKeyInput = document.getElementById("apiKey");
+const userQuestionInput = document.getElementById("userQuestion");
+const chatContainer = document.getElementById("chat");
+const form = document.querySelector(".chat-input");
